7e1cc50e661f1a40e3b3f3df1540c872dd15c1a6
chiuko <q8156697@gmail.com>
2017-02-09 17:37:30 +0800

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -22,7 +22,7 @@ class botController extends Controller
         $jsonString = file_get_contents('php://input');
         $jsonObject = json_decode($jsonString);
 
-        $bot->sendText(1500404216, 'test');
+        $bot->sendText([1500404216], 'test');
 
     }
 }
