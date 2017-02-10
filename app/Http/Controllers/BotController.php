72736c5bdd5166bd108c9998d0f1ce55d0d41701
chiuko <q8156697@gmail.com>
2017-02-10 09:32:48 +0800

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -9,17 +9,9 @@ class botController extends Controller
 
     public function callBack()
     {
-        file_put_contents("php://stderr", "sending push !!!".PHP_EOL);
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
-
-        $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-        $body = file_get_contents("php://input");
-        try {
-          $events = $bot->parseEventRequest($body, $signature);
-          file_put_contents("php://stderr", "$events".PHP_EOL);
-        } catch (Exception $e) {
-          file_put_contents("php://stderr", "$e".PHP_EOL);
+        file_put_contents("php://stderr", "sending push !!!".PHP_EOL);
         }
 
     }
