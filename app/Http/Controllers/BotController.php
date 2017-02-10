e569ff1f807541cfe94b6ca0b87bba3b0f2109cc
chiuko <q8156697@gmail.com>
2017-02-10 13:56:55 +0800

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -57,22 +57,23 @@ class botController extends Controller
         switch ($typeName) {
             case '日幣':
                 $money = $sourceData->rates->JPY;
-
-                //to string
-                $txt = "買入現金 : " . $money->buyCash;
-                $txt .= "\n";
-                $txt .= "買入即期 : " . $money->buySpot;
-                $txt .= "\n";
-                $txt .= "賣出現金 : " . $money->sellCash;
-                $txt .= "\n";
-                $txt .= "賣出即期 : " . $money->sellSpot;
-                $txt .= "\n";
-                $txt .= "更新時間 : " . Carbon::createFromTimestamp($sourceData->updateTime)->format('Y-m-d H:i:s');
-
-                return $txt;
+                break;
 
             default:
                 return '';
         }
+
+        //to string
+        $txt = "買入現金 : " . $money->buyCash;
+        $txt .= "\n";
+        $txt .= "買入即期 : " . $money->buySpot;
+        $txt .= "\n";
+        $txt .= "賣出現金 : " . $money->sellCash;
+        $txt .= "\n";
+        $txt .= "賣出即期 : " . $money->sellSpot;
+        $txt .= "\n";
+        $txt .= "更新時間 : " . Carbon::createFromTimestamp($sourceData->updateTime)->format('Y-m-d H:i:s');
+
+        return $txt;
     }
 }
