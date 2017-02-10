ad859979cd7f6b8cc40efdccf29244267ae390bf
chiuko <q8156697@gmail.com>
2017-02-10 09:46:36 +0800

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Log;
 
 class botController extends Controller
 {
@@ -14,6 +15,15 @@ class botController extends Controller
     {
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
+        Log::info('testtesttesttesttesttest');
+
+        /*$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+        $body = file_get_contents("php://input");
+        try {
+          $events = $bot->parseEventRequest($body, $signature);
+
+        } catch (Exception $e) {
+        }*/
 
     }
 }
