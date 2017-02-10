abf281e064bd5e0a8681d985d9a1df7c6c750a7d
chiuko <q8156697@gmail.com>
2017-02-10 09:34:47 +0800

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 
 class botController extends Controller
 {
+    public function test()
+    {
+    }
 
     public function callBack()
     {
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
-        file_put_contents("php://stderr", "sending push !!!".PHP_EOL);
-        }
 
     }
 }
