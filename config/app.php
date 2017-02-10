cf9657237c99383b970e699b645384f1f5783143
chiuko <q8156697@gmail.com>
2017-02-10 11:38:26 +0800

    push send

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Swap\Laravel\SwapServiceProvider::class,
 
     ],
 
@@ -226,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Swap' => Swap\Laravel\Facades\Swap::class
     ],
 
 ];
